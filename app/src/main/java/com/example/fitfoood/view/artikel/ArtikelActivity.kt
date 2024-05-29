d0b31df14ad0655b1ec27afa6ff644dcdb2727d2
Lucyta Qutsyaning Rosydah <lucytaqr08@gmail.com>
2024-05-29 09:49:25 +0700

    Buat CameraX merge

--- app/src/main/java/com/example/fitfoood/view/artikel/ArtikelActivity.kt
+++ app/src/main/java/com/example/fitfoood/view/artikel/ArtikelActivity.kt
@@ -1,12 +1,54 @@
-package com.example.fitfoood.view.artikel
+package com.example.fitfoood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.fitfoood.R
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.example.fitfoood.databinding.ActivityMainBinding
+import com.example.fitfoood.view.foodchecker.CameraActivity
 
-class ArtikelActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Permission request granted", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Permission request denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    private fun allPermissionsGranted() =
+        ContextCompat.checkSelfPermission(
+            this,
+            REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_artikel)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (!allPermissionsGranted()) {
+            requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+        }
+
+        binding.btnFoodchecker.setOnClickListener { startCameraX() }
+    }
+
+    private fun startCameraX() {
+        val intent = Intent(this, CameraActivity::class.java)
+        startActivity(intent)
+    }
+
+    companion object {
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
     }
 }
