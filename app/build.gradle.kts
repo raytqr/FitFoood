f93476f60cd360d0d0dc2774311def87fd6f6fba
Lucyta Qutsyaning Rosydah <lucytaqr08@gmail.com>
2024-05-29 09:49:25 +0700

    Buat CameraX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+//    CameraX Library
+    implementation("androidx.camera:camera-camera2:1.3.3")
+    implementation("androidx.camera:camera-lifecycle:1.3.3")
+    implementation("androidx.camera:camera-view:1.3.3")
+
 }
