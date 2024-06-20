a36b64b8cf8bf76d4c281a4692873bea9241c50a
Brillianta Zayyan Muhamad <94041454+zzayyan@users.noreply.github.com>
2024-06-20 11:37:29 +0700

    fix Default Value
    blm fix 100%

--- app/src/main/java/com/example/fitfoood/view/main/MainActivity.kt
+++ app/src/main/java/com/example/fitfoood/view/main/MainActivity.kt
@@ -1,93 +1,112 @@
 package com.example.fitfoood
 
+import ProfileFragment
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.fitfoood.databinding.ActivityMainBinding
 import com.example.fitfoood.view.foodchecker.CameraActivity
-import com.example.fitfoood.view.main.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var homeFragment: HomeFragment
+    private lateinit var profileFragment: ProfileFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Initialize fragments
+        homeFragment = HomeFragment()
+        profileFragment = ProfileFragment()
+
+        // Add homeFragment as the default fragment
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, homeFragment, "HomeFragment")
+            .add(R.id.fragment_container, profileFragment, "ProfileFragment")
+            .hide(profileFragment)  // Hide profileFragment initially
+            .commit()
+
         binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
-            var selectedFragment: Fragment? = null
             when (item.itemId) {
                 R.id.navigation_home -> {
-                    selectedFragment = HomeFragment()
+                    showFragment(homeFragment)
                     updateIcons(R.id.navigation_home)
+                    true // Return true to indicate the item is selected
                 }
                 R.id.navigation_analysis -> {
                     if (allPermissionsGranted()) {
-                        val intent = Intent(this, CameraActivity::class.java)
-                        startActivity(intent)
-                        updateIcons(R.id.navigation_analysis)
+                        startCameraActivity()
                     } else {
-                        requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+                        requestCameraPermission()
                     }
-                    return@setOnNavigationItemSelectedListener true
+                    false // Return false to indicate the item is not selected
                 }
                 R.id.navigation_profile -> {
-                    selectedFragment = ProfileFragment()
+                    showFragment(profileFragment)
                     updateIcons(R.id.navigation_profile)
+                    true // Return true to indicate the item is selected
                 }
+                else -> false // Handle unknown item
             }
-            if (selectedFragment != null) {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, selectedFragment)
-                    .commit()
-            }
-            true
         }
 
         // Set default selection
         binding.bottomNavigation.selectedItemId = R.id.navigation_home
     }
 
-    private fun updateIcons(selectedItemId: Int) {
-        val homeIcon = if (selectedItemId == R.id.navigation_home) R.drawable.ic_home_click else R.drawable.ic_home
-        val cameraIcon = if (selectedItemId == R.id.navigation_analysis) R.drawable.ic_camera_click else R.drawable.ic_camera
-        val profileIcon = if (selectedItemId == R.id.navigation_profile) R.drawable.ic_profile_click else R.drawable.ic_profile
+    private fun showFragment(fragment: Fragment) {
+        // Replace the current fragment with the selected one
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
 
-        binding.bottomNavigation.menu.findItem(R.id.navigation_home).setIcon(homeIcon)
-        binding.bottomNavigation.menu.findItem(R.id.navigation_analysis).setIcon(cameraIcon)
-        binding.bottomNavigation.menu.findItem(R.id.navigation_profile).setIcon(profileIcon)
+    private fun updateIcons(selectedItemId: Int) {
+        binding.bottomNavigation.menu.findItem(R.id.navigation_home)?.setIcon(
+            if (selectedItemId == R.id.navigation_home) R.drawable.ic_home_click else R.drawable.ic_home
+        )
+        binding.bottomNavigation.menu.findItem(R.id.navigation_analysis)?.setIcon(
+            if (selectedItemId == R.id.navigation_analysis) R.drawable.ic_camera_click else R.drawable.ic_camera
+        )
+        binding.bottomNavigation.menu.findItem(R.id.navigation_profile)?.setIcon(
+            if (selectedItemId == R.id.navigation_profile) R.drawable.ic_profile_click else R.drawable.ic_profile
+        )
+    }
+
+    private fun startCameraActivity() {
+        val intent = Intent(this, CameraActivity::class.java)
+        startActivity(intent)
+        updateIcons(R.id.navigation_analysis)
     }
 
     private val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
             if (isGranted) {
-                Toast.makeText(this, "Permission request granted", Toast.LENGTH_LONG).show()
-                val intent = Intent(this, CameraActivity::class.java)
-                startActivity(intent)
-                updateIcons(R.id.navigation_analysis)
+                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_LONG).show()
+                startCameraActivity()
             } else {
-                Toast.makeText(this, "Permission request denied", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_LONG).show()
             }
         }
 
-    private fun allPermissionsGranted() =
-        ContextCompat.checkSelfPermission(
-            this,
-            REQUIRED_PERMISSION
-        ) == PackageManager.PERMISSION_GRANTED
+    private fun requestCameraPermission() {
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+    }
 
-    companion object {
-        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
+    private fun allPermissionsGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
     }
 }
